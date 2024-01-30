ca417b2d155cacfd96378f9041ff55e5670e4133
Nikita Lemesh <nikita.lemesh.1991@mail.ru>
2024-01-30 11:27:54 +0100

    feat: add layout in index.tsx

--- react-crypto/src/App.tsx
+++ react-crypto/src/App.tsx
@@ -1,11 +1,49 @@
 import React from 'react';
+import { Layout } from 'antd';
 
+
+const headerStyle: React.CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 64,
+  paddingInline: 48,
+  lineHeight: '64px',
+  backgroundColor: '#4096ff',
+};
+
+const contentStyle: React.CSSProperties = {
+  textAlign: 'center',
+  minHeight: 120,
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#0958d9',
+};
+
+const siderStyle: React.CSSProperties = {
+  textAlign: 'center',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#1677ff',
+};
+
+const footerStyle: React.CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  backgroundColor: '#4096ff',
+};
 
 function App() {
   return (
-    <div>
-      <h1>Project</h1>
-    </div>
+    <Layout>
+      <Layout.Header style={headerStyle}>Header</Layout.Header>
+      <Layout>
+        <Layout.Sider width='25%' style={siderStyle}>
+        Sider
+        </Layout.Sider>
+        <Layout.Content style={contentStyle}>Content</Layout.Content>
+      </Layout>
+      <Layout.Footer style={footerStyle}>Footer</Layout.Footer>
+    </Layout>
   );
 }
 
