00b30ed3adb223dfa63351a2b5835962eced2180
Nikita Lemesh <nikita.lemesh.1991@mail.ru>
2024-02-01 14:06:31 +0100

    fix: add loading from AppSider to AppLayout

--- react-crypto/src/components/layout/AppLauout.tsx
+++ react-crypto/src/components/layout/AppLauout.tsx
@@ -1,9 +1,15 @@
-import { Layout } from 'antd';
+import { Layout, Spin } from 'antd';
 import { AppHeader } from './AppHeader';
 import { AppSider } from './AppSider';
 import { AppContant } from './AppContent';
+import { useContext } from 'react';
+import { CryptoContext } from '../../context/crypto-context';
 
 export const AppLayout = () => {
+  const { loading }: { loading: boolean } = useContext(CryptoContext);
+  if(loading) {
+    return <Spin fullscreen />;
+  }
   return (
     <Layout>
       <AppHeader />
