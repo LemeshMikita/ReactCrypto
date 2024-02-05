d561930f8b39dcea2701087eb6917d1fdd964c88
Nikita Lemesh <nikita.lemesh.1991@mail.ru>
2024-02-05 18:19:55 +0100

    :fix: fix inputs

--- react-crypto/src/components/AddAssetForm.tsx
+++ react-crypto/src/components/AddAssetForm.tsx
@@ -1,8 +1,19 @@
 import { useState } from 'react';
-import { Select, Space, Typography, Flex, Divider, Form, Input, Checkbox, Button, InputNumber } from 'antd';
+import { Select, Space, Typography, Flex, Divider, Form, DatePicker, Button, InputNumber } from 'antd';
 import { useCrypto } from '../context/crypto-context';
 
+const validateMessages = {
+  require: '${label} is required',
+  types: {
+    number: '${label} is not valid number',
+  },
+  number: {
+    range: '${label} must be between ${min} and ${max}'
+  }
+};
+
 export const AddAssetForm = () => {
+  const [form] = Form.useForm();
   const { crypto }: any = useCrypto();
   const [coin, setCoin]: any = useState(null);
 
@@ -29,15 +40,25 @@ export const AddAssetForm = () => {
   const onFinish = (values: any) => {
     console.log('Success:', values);
   };
+
+  const handleAmount = (value: any) => {
+    form.setFieldsValue({
+      total: (value * coin.price).toFixed(2)
+    });
+  };
   
   return (
     <Form
+      form={form}
       name='basic'
       labelCol={{ span: 4 }}
       wrapperCol={{ span: 10 }}
       style={{ maxWidth: 600 }}
-      initialValues={{}}
+      initialValues={{
+        price: coin.price.toFixed(2)
+      }}
       onFinish={onFinish}
+      validateMessages={validateMessages}
     >
       <Flex align='center'>
         <img src={coin.icon} alt='coin' style={{width: 40, marginRight: 10}} />
@@ -54,19 +75,25 @@ export const AddAssetForm = () => {
             required: true,
             type: 'number',
             min: 0,
-            message: 'Please input your password!' 
           }
         ]}
       >
-        <InputNumber style={{width: '100%'}}/>
+        <InputNumber 
+          style={{width: '100%'}}
+          onChange={handleAmount}
+        />
       </Form.Item>
 
       <Form.Item label='Price' name='price'>
-        <InputNumber style={{width: '100%'}}/>
+        <InputNumber style={{width: '100%'}} disabled/>
+      </Form.Item>
+
+      <Form.Item label='Date & Time' name='date'>
+        <DatePicker showTime/>
       </Form.Item>
 
       <Form.Item label='Total' name='total'>
-        <InputNumber style={{width: '100%'}}/>
+        <InputNumber style={{width: '100%'}} disabled/>
       </Form.Item>
 
       <Form.Item>
